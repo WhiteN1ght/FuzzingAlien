e86f3a8e13d8a25b90b85550320defecffca8c26
whiten1ght <whiten1ght@163.com>
2018-07-22 14:58:11 +0800

    Initial Commit

---
+++ AlienFuzzing/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AlienFuzzing
+//
+//  Created by 金源 on 2018/7/22.
+//  Copyright © 2018年 金源. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
